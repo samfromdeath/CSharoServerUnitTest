b13cad04bd079e1dd6fbbb6bad94ee67dea35b36
samfromdeath <samuel@intelogy.com.au>
2015-09-16 11:02:04 +1000

    Added Client Login

---
+++ CSharpServerUnitTest/ClientLogin.cs
@@ -0,0 +1,70 @@
+﻿using cSharpServer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpServerUnitTest
+{
+    class ClientLogin : IClientLogin
+    {
+        /// <summary>
+        /// This login uses a flat file system, data/users/{0}.user - the users name is the file.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public bool Login(byte[] data, Client client)
+        {
+            try
+            {
+                BinaryBuffer buff = new BinaryBuffer(data, true);
+
+                client.Username = buff.ReadString(buff.ReadInt()); // Username
+                client.Password = buff.ReadString(buff.ReadInt()); // Password
+
+                buff.EndRead();
+
+                buff = new BinaryBuffer(File.ReadAllBytes(string.Format(@"data/users/{0}.svpref", client.Username)), true);
+
+                LoadClientArguments(ref buff, ref client);
+                // does pass equal?
+
+                string arg;
+                client.GetArgument("Password", out arg);
+                if (arg != client.Password)
+                    return false;
+
+                buff.EndRead();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// This function will run not matter if it errors...
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="client"></param>
+        public void LoadClientArguments(ref BinaryBuffer data, ref Client client)
+        {
+            //how many arguments do we have stored?
+            try
+            {
+                int Total = data.ReadInt();
+                for (int i = 0; i < Total; i++)
+                    client.Arguments.Add(new ClientArgument(data.ReadString(data.ReadInt()), data.ReadByte(), data.ReadByteArray(data.ReadInt())));
+            }
+            catch (Exception)
+            {
+                // If the file is out of order, they lose there data.
+                // because if they can login then the file has been changed manually.
+            }
+        }
+    }
+}
---
+++ CSharpServerUnitTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using cSharpServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpServerUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = (Database.CurrentServer = new Server());
+
+            server.Start();
+
+            server.HandleCommands();
+        }
+    }
+}
